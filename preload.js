788137f043906311d87c89ee4f43b887f2bc892a
Yangyang.zhang <yangyang.zhang@weifu.com.cn>
2022-06-07 13:42:11 +0800

    update

--- preload.js
+++ preload.js
@@ -1,24 +1,39 @@
-const remote = require('electron').remote;
-const remote2 = require('electron')
-console.log(remote2);
+// const remote = require('electron').remote;
+
+const { remote } = require("electron");
+console.log(remote);
+console.warn(remote);
+
 // New way.
 // var app = require('electron').remote.app
 // var app = require('electron').remote.require('app')
 // var BrowserWindow = require('electron').remote.BrowserWindow
 // console.log("app",app);
-console.log(global);
- console.log('remote', remote);
-      const Menu = remote.Menu;
-      const MenuItem = remote.MenuItem;
+// console.log(global);
+//  console.log('remote', remote);
+//       const Menu = remote.Menu;
+//       const MenuItem = remote.MenuItem;
 
-      var menu = new Menu();
-      menu.append(new MenuItem({ label: 'MenuItem1', click: function () { console.log('item 1 clicked'); } }));
-      menu.append(new MenuItem({ type: 'separator' }));
-      menu.append(new MenuItem({ label: 'MenuItem2', type: 'checkbox', checked: true }));
+//       var menu = new Menu();
+//       menu.append(new MenuItem({ label: 'MenuItem1', click: function () { console.log('item 1 clicked'); } }));
+//       menu.append(new MenuItem({ type: 'separator' }));
+// menu.append(new MenuItem({ label: 'MenuItem2', type: 'checkbox', checked: true }));
+//       const { remote } = require("electron");
+// 通过remote模块调用主进程的menu模块
+const Menu = remote.Menu;
+ 
+//  配置右键菜单，按角色来配置
+const contextTemplate = [
+    { label: "复制", role: "copy" },
+    { label: "粘贴", role: "paste" },
+    { label: "剪切", role: "cut" }
+];
+var menuBuilder = Menu.buildFromTemplate(contextTemplate);
+
 
       window.addEventListener('contextmenu', function (e) {
         e.preventDefault();
-        menu.popup(remote.getCurrentWindow());
+        menuBuilder.popup(remote.getCurrentWindow());
       }, false);
 
   
