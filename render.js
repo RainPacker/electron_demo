5e611fae31c4da6ceb54e4b3e4460ae8f54ce05e
rain.zhang <254856090@qq.com>
2022-06-09 20:19:17 +0800

    更新优化

--- render.js
+++ render.js
@@ -4,6 +4,7 @@ const CLICK_MESSAGE = 'Notification clicked!'
 // const  remote   = require("electron");
 // console.log(remote);
 // console.warn(remote);
+const remote = require('@electron/remote')
 const { shell} = require("electron");
 
   
@@ -27,7 +28,29 @@ document.getElementById("notification").onclick = () => {
         shell.openPath("read -9.xlsx").then(res => { 
             console.log(res);
         })
+    }  
+    
+    document.getElementById("dailog").onclick = () => {
+         remote.dialog.showOpenDialog({
+             title: "rainzhsn",
+             filters: [{name: "img", extensions: ["jpg", "png"]}],
+             buttonLabel: "选择图片"
+         })
+    } 
+     document.getElementById("msg").onclick = () => {
+         remote.dialog.showMessageBox({
+             title: "rainzhsn",
+             type: "info",
+             message: "Are you ok ?",
+             buttons: ["ok", "fine"]
+         })
     }
 }
-
+window.addEventListener("online",()=>{
+    console.log("online");
+})
+window.addEventListener("offline",()=>{
+    console.log("offline");
+    new Notification("提醒", {body: "网络断开了"})
+})
 
